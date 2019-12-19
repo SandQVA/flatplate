8a46217760fa1bc91de57e91eee119aed7ea95e9
BERGER Sandrine <s.berger@daep-berger.isae.fr>
2019-12-19 14:21:04 +0100

    update testsimplified to use polar coordinates

--- testsimplified.py
+++ testsimplified.py
@@ -17,7 +17,6 @@ except ModuleNotFoundError:
     pass
 import torch
 
-
 import numpy as np
 
 from model import Model
@@ -43,7 +42,6 @@ with open(os.path.join(args.folder, 'configuration.yaml'), 'r') as file:
 
 device = torch.device('cpu')
 
-
 if not os.path.exists(args.folder+'/test/'):
     os.mkdir(args.folder+'/test/')
 
@@ -63,19 +61,13 @@ initialconditions = [xA,yA,uA,vA]
 STATE_SIZE = len(initialconditions)
 ACTION_SIZE = 1
 
-
 # Create gym environment
 env = FlatPlateModel(initialconditions,finalstate,config)
-
-
-
 
 # Creating neural networks and loading models
 model = Model(device, STATE_SIZE, ACTION_SIZE,args.folder, config)
 model.load()
 print("\033[91m\033[1mModel loaded from ", args.folder, "\033[0m")
-
-
 
 n_ep = config["TEST_EPISODES"]
 
@@ -114,14 +106,15 @@ try:
             rowstep += 1
             
             action = model.select_action(state)
-            state, r, done = env.step(action)
+            state, r, done = env.step(action, state)
             reward += r
             steps += 1
-            
-            xmatrix[rowstep][columnepisode] = state[0]
-            ymatrix[rowstep][columnepisode] = state[1]
-            umatrix[rowstep][columnepisode] = state[2]
-            vmatrix[rowstep][columnepisode] = state[3]
+
+            cartesian_state = env.get_state_in_absolute_cartesian_coordinates(state) 
+            xmatrix[rowstep][columnepisode] = cartesian_state[0]
+            ymatrix[rowstep][columnepisode] = cartesian_state[1]
+            umatrix[rowstep][columnepisode] = cartesian_state[2]
+            vmatrix[rowstep][columnepisode] = cartesian_state[3]
             
             actionsmatrix[rowstep][columnepisode] = action[0]*180/np.pi
             rewardsmatrix[rowstep][columnepisode] = r
@@ -129,7 +122,6 @@ try:
         rewards.append(reward)
         print('Episode reward:',reward)
         columnepisode += 1
-        
         
         
 except KeyboardInterrupt:
@@ -143,15 +135,10 @@ finally:
     np.savetxt(str(args.folder)+'/test/actions.csv', actionsmatrix[:,0:n_ep], delimiter=";")
     np.savetxt(str(args.folder)+'/test/rewards.csv', rewardsmatrix[:,0:n_ep], delimiter=";")
     
-    
     if rewards:
         score = sum(rewards)/len(rewards)
     else:
         score = 0
 
-
 print(f"Average score : {score}")
-
-
 #score = model.evaluate(env,n_ep=args.nb_tests)#, render=args.render, gif=args.gif)
-
